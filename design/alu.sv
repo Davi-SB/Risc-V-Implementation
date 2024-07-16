1e009272a1f7e00aa9e3afa4e19fbd11f17fc8fa
Davi <davibrilhante0102@gmail.com>
2024-07-16 17:29:20 -0300

    primeiro dia

--- design/alu.sv
+++ design/alu.sv
@@ -1,29 +1,31 @@
 `timescale 1ns / 1ps
 
-module alu#(
-        parameter DATA_WIDTH = 32,
-        parameter OPCODE_LENGTH = 4
-        )
+module alu#(parameter DATA_WIDTH = 32, parameter OPCODE_LENGTH = 4)
         (
-        input logic [DATA_WIDTH-1:0]    SrcA,
-        input logic [DATA_WIDTH-1:0]    SrcB,
-
-        input logic [OPCODE_LENGTH-1:0]    Operation,
-        output logic[DATA_WIDTH-1:0] ALUResult
+        input  logic [DATA_WIDTH-1:0]    SrcA,
+        input  logic [DATA_WIDTH-1:0]    SrcB,
+        input  logic [OPCODE_LENGTH-1:0] Operation,
+        output logic [DATA_WIDTH-1:0]    ALUResult
         );
     
-        always_comb
-        begin
-            case(Operation)
-            4'b0000:        // AND
-                    ALUResult = SrcA & SrcB;
-            4'b0010:        // ADD
-                    ALUResult = $signed(SrcA) + $signed(SrcB);
-            4'b1000:        // Equal
-                    ALUResult = (SrcA == SrcB) ? 1 : 0;
-            default:
-                    ALUResult = 0;
-            endcase
-        end
+    always_comb begin
+        case(Operation)
+            4'b0000: ALUResult = SrcA & SrcB;                    // AND
+            4'b0001: ALUResult = SrcA | SrcB;                    // OR
+            4'b0010: ALUResult = $signed(SrcA) + $signed(SrcB);  // ADD, ADDI
+            4'b0011: ALUResult = $signed(SrcA) - $signed(SrcB);  // SUB
+            4'b0100: ALUResult = SrcA << SrcB;                   // SLL
+            4'b0101: ALUResult = SrcA >> SrcB;                   // SRL
+            4'b0110: ALUResult = SrcA ^ SrcB;                    // XOR
+            4'b0111: ALUResult = $signed(SrcA) >>> SrcB;         // SRA
+            4'b1000: ALUResult = (SrcA == SrcB) ? 1 : 0;         // BEQ
+            4'b1001: ALUResult = (SrcA != SrcB) ? 1 : 0;         // BNE
+            4'b1010: ALUResult = (SrcA < SrcB)  ? 1 : 0;         // BLT
+            4'b1011: ALUResult = (SrcA >= SrcB) ? 1 : 0;         // BGE
+            4'b1100: ALUResult = (SrcA < SrcB)  ? 1 : 0;         // SLTI
+            4'b1111: ALUResult = SrcB;                           // LUI
+            default: ALUResult = 0;
+        endcase
+    end
 endmodule
 
--- design/datamemory.sv
+++ design/datamemory.sv
@@ -29,19 +29,29 @@ module datamemory #(
   );
 
   always_ff @(*) begin
-    raddress = {{22{1'b0}}, a[8:2], {2{1'b0}}};
+    raddress = {{22{1'b0}}, a};
     waddress = {{22{1'b0}}, a[8:2], {2{1'b0}}};
     Datain = wd;
     Wr = 4'b0000;
 
     if (MemRead) begin
       case (Funct3)
-        3'b010:  //LW
-        rd <= Dataout;
+        3'b000:  rd <= $signed(Dataout[7:0]);  //LB
+        3'b001:  rd <= $signed(Dataout[15:0]);  //LH
+        3'b010:  rd <= Dataout;  //LW
+        3'b100:  rd <= {24'b0, Dataout[7:0]};  //LBU
         default: rd <= Dataout;
       endcase
     end else if (MemWrite) begin
       case (Funct3)
+        3'b000: begin  //SB
+          Wr <= 4'b0100;
+          Datain[7:0] = wd;
+        end
+        3'b001: begin  //SH   
+          Wr <= 4'b1100;
+          Datain[15:0] = wd;
+        end
         3'b010: begin  //SW
           Wr <= 4'b1111;
           Datain <= wd;
